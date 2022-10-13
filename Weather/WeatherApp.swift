b6ae51ea41eade10fc523af55a4e778cc633f5cd
Szymon Wnuk <szymon.wnuk@icloud.com>
2022-10-13 15:46:11 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Szymon Wnuk on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
